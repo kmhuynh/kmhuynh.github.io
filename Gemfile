bb284b65b8bd06dbfded540a60173731263f77a4
Khoi Huynh <khoihminh@gmail.com>
2020-09-17 20:18:42 -0400

    NA

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ source "https://rubygems.org"
 # Happy Jekylling!
 
 gem "github-pages", group: :jekyll_plugins
-gem "jekyll", "~> 3.5"
+#gem "jekyll", "~> 3.5"
 #gem "minimal-mistakes-jekyll"
 
 # If you want to use Jekyll native, uncomment the line below.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "PhD Comics",
+        "excerpt":"If you reach here, you are either having too much free time or procrastinating. In the latter case, enjoy this piece from PhD Comics.   Disclaimer: your PI might not like it.     ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/posts/2020/09/PhDComics/",
+        "teaser": null
+      },]
