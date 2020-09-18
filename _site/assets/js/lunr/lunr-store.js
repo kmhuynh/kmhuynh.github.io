0f44c2d1e581ebc3542e7ebfc98d12751735a8c1
Khoi Huynh <khoihminh@gmail.com>
2020-09-17 23:47:07 -0400

    NA

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "PhD Comics",
-        "excerpt":"If you reach here, you are either having too much free time or procrastinating. In the latter case, enjoy this piece from PhD Comics.   Disclaimer: your PI might not like it.     ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/posts/2020/09/PhDComics/",
-        "teaser": null
-      },]
